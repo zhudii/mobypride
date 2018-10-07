9cfabdb4753e8c03ce678f38c1cce70fd735e33d
zhudii <34613623+zhudii@users.noreply.github.com>
2018-10-07 18:11:11 -0400

    Update quoteswap.py

--- quoteswap.py
+++ quoteswap.py
@@ -16,9 +16,12 @@ pride = pride[629:]
 mobyquotes = re.findall('"[^"]*\r\n\r\n|"[^"]*"', moby)
 pridequotes = re.findall('"[^"]*\r\n\r\n|"[^"]*"', pride)
 
-def quoteswap(mobyquotes, pridequotes, moby):
-    for i in range(len(pridequotes[:1535])):
-        moby = moby.replace(mobyquotes[i], pridequotes[i])
-    return moby
+def quoteswap(quotes1, quotes2, text1):
+    for i in range(len(quotes2[:1535])):
+        text1 = text1.replace(quotes1[i], quotes2[i])
+    return text1
     
+# if you want to replace the quotes in Moby Dick with the quotes in Pride and Prejudice
 print(quoteswap(mobyquotes, pridequotes, moby))
+# if you want to replace the quotes in Pride and Prejudice with the quotes in Moby Dick
+print(quoteswap(pridequotes, mobyquotes, pride))
